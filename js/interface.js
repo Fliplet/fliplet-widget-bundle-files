02875358946a2b3510d1e3857a61fd9498f2d1b1
Nicholas Valbusa <squallstar@gmail.com>
2018-03-01 15:06:28 +0100

    tweaks

--- js/interface.js
+++ js/interface.js
@@ -20,21 +20,40 @@ Fliplet().then(function () {
     event.preventDefault();
 
     var txt = $('textarea').val();
-    data.files = txt.split('\n').map(function (url, idx) {
+
+    $('.result').html('<div class="alert alert-info" role="alert">Validating files...</div>');
+
+    Promise.all(txt.split('\n').map(function (url, idx) {
       url = url.trim();
+
+      if (!url.match(/^https?:\/\/.+/)) {
+        return Promise.reject('The URL "' + url + '" needs to start with http:// or https://.');
+      }
+
+      var parts = url.split('?');
+      url = parts[0];
       var pieces = url.split('.');
+      var extension = (_.last(pieces) || '').toLowerCase();
+
+      // TODO: Use API proxy to check whether links are valid and what's their content type
+
+      if (['css', 'js'].indexOf(extension) === -1) {
+        return Promise.reject('The URL "' + url + '" needs to reference a file ending with .css or .js extension.');
+      }
 
       return {
         url: url,
-        path: 'bundle/' + idx + '.' + pieces[pieces.length-1],
+        path: 'bundle/' + idx + '.' + extension,
         updatedAt: Math.round(Date.now()/1000)
       };
-    });
-
-    $('.result').html('');
-
-    Fliplet.Widget.save(data).then(function () {
-      $('.result').html('<div class="alert alert-success" role="alert">Done! Your app bundle has been updated.</div>');
+    })).then(function (files) {
+      Fliplet.Widget.save({
+        files: files
+      }).then(function () {
+        $('.result').html('<div class="alert alert-success" role="alert">Done! Your app bundle has been updated.</div>');
+      });
+    }).catch(function (error) {
+      $('.result').html('<div class="alert alert-danger" role="alert">' + error + '</div>');
     });
   });
 
@@ -42,4 +61,6 @@ Fliplet().then(function () {
   Fliplet.Widget.onSaveRequest(function () {
     $('form').submit();
   });
+
+  Fliplet.Widget.autosize();
 });
